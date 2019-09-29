8a4d099c36c2cb13fe02fee57dab3ec20cb36086
lombardero <manuel.lombardero.botey@gmail.com>
2019-09-28 23:09:30 -0400

    update img_scav

--- models/img_scav.py
+++ models/img_scav.py
@@ -5,6 +5,8 @@ from watson_developer_cloud import VisualRecognitionV3
 visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     iam_apikey='ejliqAzwzws4ICSiOthvI0MSUmal-R00BmistPaYAV50')
+# check_class receives an input the name of a JPG file in /assets and returns its
+# predicted category: "No_class", "MLH_table" or "Courant_paint"
 def check_class(filename):
     PATH = os.getcwd() + '/assets/'+filename+'.jpg'
     print(PATH)
@@ -13,5 +15,8 @@ def check_class(filename):
             images_file,
             threshold='0.6',
         classifier_ids='Boomba_v0_1140204870').get_result()
-    #print(json.dumps(classes, indent=2))
-    return classes['images'][0]['classifiers'][0]['classes'][0]
+    out = classes['images'][0]['classifiers'][0]['classes']
+    if out == []:
+        return 'No_class'
+    else:
+        return out[0]['class']
